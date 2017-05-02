efe571318d7e921e86e8ff320a07a010a85c96e6
Yevhen <piskuntech@gmail.com>
2017-05-02 23:42:25 +0300

    add shuffle method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("New game");
-        Card card = new Card(Suit.CLUBS, Rank.ACE);
         Deck deck = new Deck();
         deck.createDeck();
+        deck.shuffle();
 
     }
 }
