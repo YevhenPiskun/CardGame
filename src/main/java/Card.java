9f23f5c2db172f9e1748a5711535b35fb1e1dd99
Yevhen <piskuntech@gmail.com>
2017-05-02 13:32:56 +0300

    change class Card and add new class Deck

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -2,4 +2,24 @@
 public class Card {
     private Suit suit;
     private Rank rank;
+
+    Card(Suit suit, Rank rank){
+    }
+
+    public Suit getSuit(){
+        return suit;
+    }
+
+    public Rank getRank(){
+        return rank;
+    }
+
+    public void setSuit(Suit suit){
+        this.suit = suit;
+    }
+
+    public void setRank(Rank rank){
+        this.rank = rank;
+    }
+
 }
